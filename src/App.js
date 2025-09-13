c997f911bc73a243b58a4a4fa6a52c566653d166
kostyachaika <kostya.c@allsetnow.com>
2025-09-13 23:56:01 +0200

    Fix App.js for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,101 +1,19 @@
 import {
   createHashRouter,
   RouterProvider,
-  Outlet,
   createRoutesFromElements,
   Route,
-  ScrollRestoration,
 } from "react-router-dom";
-
-import { store, persistor } from "./redux/store";
-import { ToastProvider } from "./contexts/ToastContext";
-import { AuthProvider } from "./contexts/AuthContext";
-import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
-import Footer from "./components/home/Footer/Footer";
-import FooterBottom from "./components/home/Footer/FooterBottom";
-import Header from "./components/home/Header/Header";
-import HeaderBottom from "./components/home/Header/HeaderBottom";
-import SpecialCase from "./components/SpecialCase/SpecialCase";
-import ProtectedRoute from "./components/ProtectedRoute";
-import About from "./pages/About/About";
-import SignIn from "./pages/Account/SignIn";
-import SignUp from "./pages/Account/SignUp";
-import Cart from "./pages/Cart/Cart";
-import Contact from "./pages/Contact/Contact";
 import Home from "./pages/Home/Home";
-import Offer from "./pages/Offer/Offer";
-import Payment from "./pages/payment/Payment";
-import ProductDetails from "./pages/ProductDetails/ProductDetails";
-import Shop from "./pages/Shop/Shop";
-import Profile from "./pages/Profile/Profile";
-import Orders from "./pages/Orders/Orders";
-import Settings from "./pages/Settings/Settings";
 
-const Layout = () => {
-  return (
-    <div>
-      <Header />
-      <HeaderBottom />
-      <SpecialCase />
-      <ScrollRestoration />
-      <Outlet />
-      <Footer />
-      <FooterBottom />
-    </div>
-  );
-};
 const router = createHashRouter(
   createRoutesFromElements(
-    <Route>
-      <Route path="/" element={<Layout />}>
-        {/* ==================== Header Navlink Start here =================== */}
-        <Route index element={<Home />}></Route>
-        <Route path="/shop" element={<Shop />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/contact" element={<Contact />}></Route>
-        {/* ==================== Header Navlink End here ===================== */}
-        <Route path="/offer" element={<Offer />}></Route>
-        <Route path="/product/:_id" element={<ProductDetails />}></Route>
-        <Route path="/cart" element={<Cart />}></Route>
-        <Route path="/paymentgateway" element={
-          <ProtectedRoute>
-            <Payment />
-          </ProtectedRoute>
-        }></Route>
-        <Route path="/profile" element={
-          <ProtectedRoute>
-            <Profile />
-          </ProtectedRoute>
-        }></Route>
-        <Route path="/orders" element={
-          <ProtectedRoute>
-            <Orders />
-          </ProtectedRoute>
-        }></Route>
-        <Route path="/settings" element={
-          <ProtectedRoute>
-            <Settings />
-          </ProtectedRoute>
-        }></Route>
-      </Route>
-      <Route path="/signup" element={<SignUp />}></Route>
-      <Route path="/signin" element={<SignIn />}></Route>
-    </Route>
+    <Route path="/" element={<Home />} />
   )
 );
 
 function App() {
-  return (
-  //  <ErrorBoundary>
-      <AuthProvider>
-        <ToastProvider>
-          <div className="font-bodyFont">
-            <RouterProvider router={router} />
-          </div>
-        </ToastProvider>
-      </AuthProvider>
-   // </ErrorBoundary>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { PersistGate } from "redux-persist/integration/react";
-import "slick-carousel/slick/slick.css";
-import { Provider } from "react-redux";
-import { store, persistor } from "./redux/store";
-import "./index.css";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </PersistGate>
-  </Provider>
-);
+root.render(<App />);
