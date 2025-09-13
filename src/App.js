b967f5a61a5f86acfdf1b4a23d6714a18458583d
kostyachaika <kostya.c@allsetnow.com>
2025-09-14 00:16:25 +0200

    Fix App.js for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,120 @@
+import React from "react";
 import {
   createHashRouter,
   RouterProvider,
+  Outlet,
   createRoutesFromElements,
   Route,
+  ScrollRestoration,
 } from "react-router-dom";
+
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store";
+
+import { ToastProvider } from "./contexts/ToastContext";
+import { AuthProvider } from "./contexts/AuthContext";
+
+import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
+import Header from "./components/home/Header/Header";
+import HeaderBottom from "./components/home/Header/HeaderBottom";
+import Footer from "./components/home/Footer/Footer";
+import FooterBottom from "./components/home/Footer/FooterBottom";
+import SpecialCase from "./components/SpecialCase/SpecialCase";
+import ProtectedRoute from "./components/ProtectedRoute";
+
 import Home from "./pages/Home/Home";
+import Shop from "./pages/Shop/Shop";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
+import SignIn from "./pages/Account/SignIn";
+import SignUp from "./pages/Account/SignUp";
+import ProductDetails from "./pages/ProductDetails/ProductDetails";
+import Cart from "./pages/Cart/Cart";
+import Payment from "./pages/payment/Payment";
+import Profile from "./pages/Profile/Profile";
+import Orders from "./pages/Orders/Orders";
+import Settings from "./pages/Settings/Settings";
+import Offer from "./pages/Offer/Offer";
+
+const Layout = () => (
+  <div>
+    <Header />
+    <HeaderBottom />
+    <SpecialCase />
+    <ScrollRestoration />
+    <Outlet />
+    <Footer />
+    <FooterBottom />
+  </div>
+);
 
 const router = createHashRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Home />} />
+    <Route>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/offer" element={<Offer />} />
+        <Route path="/product/:_id" element={<ProductDetails />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route
+          path="/paymentgateway"
+          element={
+            <ProtectedRoute>
+              <Payment />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/orders"
+          element={
+            <ProtectedRoute>
+              <Orders />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/settings"
+          element={
+            <ProtectedRoute>
+              <Settings />
+            </ProtectedRoute>
+          }
+        />
+      </Route>
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/signin" element={<SignIn />} />
+    </Route>
   )
 );
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <ErrorBoundary>
+          <AuthProvider>
+            <ToastProvider>
+              <div className="font-bodyFont">
+                <RouterProvider router={router} />
+              </div>
+            </ToastProvider>
+          </AuthProvider>
+        </ErrorBoundary>
+      </PersistGate>
+    </Provider>
+  );
 }
 
 export default App;
